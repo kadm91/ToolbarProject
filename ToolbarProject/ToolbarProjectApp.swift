02904bcfa753303dcc1d2b590944cb569bc358f8
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2023-12-31 20:53:31 -0700

    Initial Commit

---
+++ ToolbarProject/ToolbarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarProjectApp.swift
+//  ToolbarProject
+//
+//  Created by Kevin Martinez on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
