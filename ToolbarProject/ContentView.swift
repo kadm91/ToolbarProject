9b15b91033d1172bcbee6844780d0dd3fdab2b4e
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2023-12-31 20:58:35 -0700

    Create a toolbar

--- ToolbarProject/ContentView.swift
+++ ToolbarProject/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Text("Initial View")
+                .toolbar {
+                    ToolbarItem {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "gear")
+                        }
+                    }
+                }
         }
-        .padding()
     }
 }
 
